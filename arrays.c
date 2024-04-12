d4970eb598a5a83a083e43aceb67872547026bc0
DBeewms <dbwcontrol05@gmail.com>
2024-04-12 07:36:55 -0600

    Practica de arreglos en C

---
+++ arrays.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int numbers[5];
+
+    numbers[0] = 2;
+    numbers[1] = 55;
+    numbers[2] = 67;
+    numbers[3] = 98;
+    numbers[4] = 135;
+
+    printf("El elemento #1 del arreglo es %i\n", numbers[0]);
+    printf("El elemento #2 del arreglo es %i\n", numbers[1]);
+    printf("El elemento #3 del arreglo es %i\n", numbers[2]);
+    printf("El elemento #4 del arreglo es %i\n", numbers[3]);
+    printf("El elemento #5 del arreglo es %i\n", numbers[4]);
+
+
+    return 0;
+}
